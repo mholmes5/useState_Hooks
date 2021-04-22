099b1a131b7e468dbe77c43cff4d22756dd6b269
Michael Holmes <michaelholmes618@gmail.com>
2021-04-22 17:58:01 -0500

    setup app.jsx and App

--- src/index.js
+++ src/index.js
@@ -2,16 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
-var count = 0;
-
-function increase() {
-  console.log("I got clicked");
-}
-
-ReactDOM.render(
-  <div className="container">
-    <h1>{count}</h1>
-    <button onClick={increase}>+</button>
-  </div>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
