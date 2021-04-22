d0e3d0a9c74968a55e7d8b54ae577c92dbc721d4
Michael Holmes <michaelholmes618@gmail.com>
2021-04-22 18:13:11 -0500

    complete hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,21 @@
 import React, { useState } from "react";
 
 function App() {
-  const state = useState();
-  var count = 0;
+  const [count, setCount] = useState(0);
 
   function increase() {
-    count++;
+    setCount(count + 1);
+  }
+
+  function decrease() {
+    setCount(count - 1);
   }
 
   return (
     <div className="container">
       <h1>{count}</h1>
       <button onClick={increase}>+</button>
+      <button onClick={decrease}>-</button>
     </div>
   );
 }
